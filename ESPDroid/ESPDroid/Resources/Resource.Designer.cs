8c68089e0a0bdd42024eea05fe47557152acb3bf
Matt <Matt@MATT-DESKTOP>
2016-08-29 16:30:04 +1000

    Initial Commit

--- ESPDroid/ESPDroid/Resources/Resource.Designer.cs
+++ ESPDroid/ESPDroid/Resources/Resource.Designer.cs
@@ -45,13 +45,16 @@ namespace ESPDroid
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int homeImage = 2130837504;
+			public const int Guide = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int homeImage2 = 2130837505;
+			public const int homeImage = 2130837505;
 			
 			// aapt resource value: 0x7f020002
-			public const int splash = 2130837506;
+			public const int homeImage2 = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int splash = 2130837507;
 			
 			static Drawable()
 			{
